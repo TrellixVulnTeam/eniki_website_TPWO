d1c500af2c5832b4645de25eff4c64f5815ce82f
NurKhab <nurkhabib2006@gmail.com>
2022-03-05 22:15:38 +0300

    First

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -49,4 +49,4 @@ export const images = () => {
         .pipe(app.gulp.src(app.path.src.svg))
         .pipe(app.gulp.dest(app.path.build.images))
         .pipe(app.plugins.browsersync.stream());
-}
+};
